13f9101e2bf5eabaf49b06e7b10b804d9acccef9
Dean Kerr <dean_k@live.co.uk>
2016-01-05 16:25:17 +0000

    Fixed error being thrown when fully matching an order

--- server/app/matcher.js
+++ server/app/matcher.js
@@ -9,11 +9,13 @@ function Matcher() {
     this.bidOrders = []; // sorted lowest to highest price (best offer)
     this.askOrders = []; // sorted highest to lowest price (best offer)
 
-    this.trades = []; // TODO: implement trades
+    this.trades = [];
 }
 
 Matcher.prototype.onNewOrder = function(order) {
-    this.match(order, order.isBid() ? this.askOrders : this.bidOrders);
+    var matched = this.match(order, order.isBid() ? this.askOrders : this.bidOrders);
+
+    //TODO: what if not matched?
 };
 
 /**
@@ -24,12 +26,19 @@ Matcher.prototype.onNewOrder = function(order) {
  * @returns {boolean} If order has been fully matched
  */
 Matcher.prototype.match = function(order, candidates) {
-    while(order.canMatch(candidates[0]) && order.quantity > 0) {
+    var hasQuantityRemaining = true;
+
+    while(hasQuantityRemaining && !!candidates[0] && order.canMatch(candidates[0])) {
         var toMatch = candidates[0];
 
         if(order.quantity >= toMatch.quantity) {
             candidates.splice(0, 1); // fully matched, remove
-            order = order.reduceQuantity(toMatch.quantity);
+
+            if(order.quantity > toMatch.quantity) {
+                order = order.reduceQuantity(toMatch.quantity);
+            } else {
+                hasQuantityRemaining = false;
+            }
         } else {
             // partially matched, reduce quantity
             candidates[0] = new Order(toMatch.action, toMatch.price, toMatch.quantity - order.quantity);
